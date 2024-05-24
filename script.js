f4e943fd81153c40577bdfb2d710b7c292a8d3a3
DiegoEBV <diegoballonvi@gmail.com>
2024-05-24 17:52:04 -0500

    refactor: se movio la seccion de hero al main y se colocó id para poder hacer el cambio de contenido con la barra de navegación. En el archivo script.js se coloco la función para poder cambiar el contenido al hacer click en la barra de navegación

--- script.js
+++ script.js
@@ -99,3 +99,15 @@ function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+	target.addEventListener('click', () => {
+		content.forEach(c => {
+			c.classList.remove('active')
+		})
+		const t = document.querySelector(target.dataset.target)
+		t.classList.add('active')
+	})
+})
